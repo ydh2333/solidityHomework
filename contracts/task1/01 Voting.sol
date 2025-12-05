54bf2127cf6c0778cd7683ff00bd6939d681f26b
ydh2333 <1064723285@QQ.com>
2025-12-05 16:19:01 +0800

    第一题

--- contracts/task1/01 Voting.sol
+++ contracts/task1/01 Voting.sol
@@ -2,5 +2,29 @@
 pragma solidity ^0.8.0;
 
 contract Voting {
-    
+    mapping(address => bool) public voteExist;
+    mapping(address => uint) public voteCount;
+    address[] public roles;
+
+
+    function vote(address _address) public {
+        if (!voteExist[_address]){
+            voteExist[_address] = true;
+            roles.push(_address);
+        }
+        voteCount[_address] += 1;
+    }
+
+    function getVotes(address _address) public view returns (uint){
+        return voteCount[_address];
+    }
+
+    function resetVotes() public {
+        for (uint i = 0; i < roles.length;){
+            delete voteCount[roles[i]];
+            unchecked{
+                i++;
+            }
+        }
+    }
 }
